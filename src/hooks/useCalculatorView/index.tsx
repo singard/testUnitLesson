95637f907611f3b2ce5145e73d12eabc6ed19c45
poohia <poohia.studio@gmail.com>
2022-02-25 09:55:01 +0100

    debug setResult(

--- src/hooks/useCalculatorView/index.tsx
+++ src/hooks/useCalculatorView/index.tsx
@@ -16,22 +16,22 @@ const useCalculatorView = () => {
       const values = result.split("-");
       const value1 = values[0];
       const value2 = values[1];
-      substraction(value1, value2);
+      setResult(substraction(value1, value2));
     } else if (result.includes("/")) {
       const values = result.split("/");
       const value1 = values[0];
       const value2 = values[1];
-      division(value1, value2);
+      setResult(division(value1, value2));
     } else if (result.includes("%")) {
       const values = result.split("%");
       const value1 = values[0];
       const value2 = values[1];
-      modulo(value1, value2);
+      setResult(modulo(value1, value2));
     } else if (result.includes("√x")) {
       const values = result.split("√x");
       const value1 = values[0];
       const value2 = values[1];
-      square(value1, value2);
+      setResult(square(value1, value2));
     }
   };
 
